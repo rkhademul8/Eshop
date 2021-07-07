ffd81e5040cd9fb008eed9955f22205b7296d02d
rkhademul8 <75939920+rkhademul8@users.noreply.github.com>
2021-07-07 10:47:38 +0600

    product_and_category_tb_create

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import product, Category
 # Register your models here.
+
+admin.site.register(product)
+admin.site.register(Category)
--- store/models.py
+++ store/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Category(models.Model):
+    name=models.CharField(max_length=20)
+
+
+class product(models.Model):
+    name=models.CharField(max_length=50)
+    price=models.IntegerField(default=0)
+    category=models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description=models.CharField(max_length=200, default='')
+    image=models.ImageField(upload_to='upload/products/')
