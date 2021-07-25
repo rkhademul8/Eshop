20a866abd7b8141d57118a7e1ee03fbf634b1062
rkhademul8 <75939920+rkhademul8@users.noreply.github.com>
2021-07-25 10:09:51 +0600

    error_handling_done

--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Category, product, Customer
 # Create your views here.
@@ -32,6 +32,12 @@ def signup(request):
         phone=postData.get('numer')
         email=postData.get('email')
         password=postData.get('password')
+        value={
+            'first_name':first_name,
+            'last_name':last_name,
+            'phone':phone,
+            'email':email
+        }
 
 
         #validation
@@ -39,14 +45,34 @@ def signup(request):
         if(not first_name):
             error_message="First Name Required"
         elif len(first_name)<4:
-           
-            error_message='First Name must be 4 char or more'
+            error_message="First Name must be 4 Char log or more"
+        
+        elif not last_name:
+            error_message='Last Name Required'
+        elif len(last_name)<4:
+            error_message="Last Name must be 4 Char log or more"
+        elif not phone:
+            error_message="Phone Number Required"
+        elif len(phone)<11:
+            error_message="Phone number must be 11 Digit"
+        elif not password:
+            error_message="password is required"
+        elif len(password)<6:
+            error_message="Password must be 6 char"
+
         #save
         if not error_message:
 
         # print(first_name, last_name, phone, email, password)
             customer=Customer(first_name=first_name, last_name=last_name,phone=phone,email=email,password=password)
             customer.register()
+            return redirect('index')
         
         else:
-            return render(request,'signup.html', {'error': error_message} )
+            data={
+                'error': error_message,
+                'values':value,
+                
+
+            }
+            return render(request,'signup.html', data )
